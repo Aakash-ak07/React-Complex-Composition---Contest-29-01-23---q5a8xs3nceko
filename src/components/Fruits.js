ae38f6ba32cae4ed2e97a3f2c124b6318cecb829
Aakash Manihar <111000108+Aakash-ak07@users.noreply.github.com>
2023-01-29 15:48:21 +0530

    commit

---
+++ src/components/Fruits.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import TypesOfFruits from './TypesOfFruits';
+
+export default class Fruits extends Component{
+    render(){
+        return(
+            <TypesOfFruits />
+        )
+    }
+}
+
---
+++ src/components/TypesOfFruits.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+export default class TypesOfFruit extends Component{
+    render(){
+        return(
+            <div>TypesOfFruit</div>
+        )
+    }
+}
